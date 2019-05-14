f415a17c829a87df0e9cb4101afec0aaa2fe3100
Papooch <papoochcz@gmail.com>
2019-05-14 22:22:47 +0200

    bfs working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, time
 import pygame as pg
 
 from puzzle import *
@@ -10,9 +10,12 @@ def main():
     pg.display.set_caption('Puzzle')
     screen = pg.display.set_mode((300,300))
     clock = pg.time.Clock()
-    puzzle = Puzzle((2, 3), (300, 300), 5)
-    solver = PuzzleSolver(puzzle.gridSize)
-    solver.print()
+    puzzle = Puzzle((3, 3), (300, 300), 5)
+    finalTiles = puzzle.tiles.copy()
+    #solver = PuzzleSolver(puzzle.gridSize, puzzle.tiles, puzzle.tiles)
+    #solver.print()
+    path = []
+
     print(puzzle.tiles)
 
     while True:
@@ -21,6 +24,18 @@ def main():
         screen.fill((0,0,0))
         puzzle.draw(screen)
         pg.display.flip()
+
+        if path:
+            move = path.pop()
+            if move == 'U':
+                puzzle.move(pg.K_UP)
+            elif move == 'D':
+                puzzle.move(pg.K_DOWN)
+            elif move == 'L':
+                puzzle.move(pg.K_LEFT)
+            elif move == 'R':
+                puzzle.move(pg.K_RIGHT)
+            time.sleep(.1)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -34,7 +49,12 @@ def main():
                     puzzle.move(event.key)
                 if event.key == pg.K_SPACE:
                     puzzle.random()
-                solver.reconstructGrid(puzzle.tiles)
+                if event.key == pg.K_RETURN:
+                    solver = PuzzleSolver(puzzle.gridSize, puzzle.tiles, finalTiles)
+                    path = solver.bfs()
+
+
+
 
             
 
--- solver.py
+++ solver.py
@@ -1,17 +1,38 @@
 import numpy as np
+from collections import deque
 
 class State:
-   def __init__(self, )
-      pass
+   def __init__(self, grid, prev = None, move = None):
+      self.grid = grid
+      self.prev = prev
+      self.move = move
 
 class PuzzleSolver:
-   def __init__(self, gridSize):
+   def __init__(self, gridSize, initialtiles, finaltiles):
       self.gridSize = gridSize
-      self.grid = np.zeros((gridSize[1], gridSize[0]), dtype='int')
-      self.open = []
+      self.grid = self.reconstructGrid(initialtiles)
+      self.open = deque()
       self.closed = []
+      self.finalState = State(self.reconstructGrid(finaltiles))
 
-   def neighborState(self, state):
+
+   def inGrid(self, tile):
+      return tile[0]>=0 and tile[0]<self.gridSize[1] and tile[1]>=0 and tile[1]<self.gridSize[0]
+
+
+   def swap(self, grid, a, b):
+      grid = grid.copy()
+      tmp = grid[a]
+      grid[a] = grid[b]
+      grid[b] = tmp
+      return grid
+
+
+   def getNeighbors(self, state):
+      #print("neighbors of:\n", state.grid)
+      y, x = np.where(state.grid == 0) # coords of empty tile
+      directions = [a for a in (("D", (y-1, x)), ("U", (y+1, x)), ("R", (y, x-1)), ("L", (y, x+1))) if self.inGrid(a[1])]
+      return [State(self.swap(state.grid, dir[1], (y, x)), state, dir[0]) for dir in directions]
 
 
    def reconstructGrid(self, tiles):
@@ -19,8 +40,62 @@ class PuzzleSolver:
       for i in range(self.gridSize[0]*self.gridSize[1]):
          grid[tuple(reversed(tiles[i]))] = i+1
       grid[tuple(reversed(tiles[i]))] = 0
-      print(grid)
+      return grid
+
+
+   def inList(self, state, statelist):
+      for s in statelist:
+         if (state.grid == s.grid).all():
+            return True
+      return False
 
 
    def printGrid(self):
       print(self.grid)
+
+
+   def bfs(self):
+      self.open.append(State(self.grid))
+      
+      while self.open:
+         state = self.open.popleft()
+         self.closed.append(state)
+
+         if (state.grid == self.finalState.grid).all():
+            path = []
+            while(state.prev != None):
+               path.append(state.move)
+               state = state.prev
+            return path
+
+         neighbors = self.getNeighbors(state)
+         for neighbor in neighbors:
+            if self.inList(neighbor, self.open) or self.inList(neighbor, self.closed):
+               continue
+            self.open.append(neighbor)
+
+
+
+
+
+
+if __name__ == "__main__":
+
+   finaltiles = [(0, 0), (1, 0), (0, 1), (1, 1), (0, 2), (1, 2)]
+   tiles = [(1, 2), (0, 2), (0, 1), (0, 0), (1, 0), (1, 1)]
+   #tiles = [(0, 1), (0, 0), (1, 1), (1, 2), (0, 2), (1, 0)]
+   #finaltiles = [(0, 0), (1, 0), (1, 1), (0, 1)]
+   #tiles = [(1, 0), (1, 1), (0, 1), (0, 0)]
+
+   solver = PuzzleSolver((2, 3), tiles, finaltiles)
+   neighbors = (solver.getNeighbors(State(solver.grid)))
+
+   print(solver.grid, "\n")
+
+   for neighbor in neighbors:
+      print(neighbor.grid, "\n")
+   
+   path = solver.bfs()
+   print(path)
+
+
